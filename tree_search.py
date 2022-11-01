7bea5dc3b5b0ed8a8c63e2105b54aa7c63a71f28
Barbora Barancikova <2439071b@student.gla.ac.uk>
2022-11-01 11:10:26 +0100

    Add tree search timeout

--- tree_search.py
+++ tree_search.py
@@ -5,6 +5,7 @@ from RL_value_function import get_value
 from RL_state_reward import is_dead, state_reward
 from state_generator import cleanup_state
 from state_generator import next_state_for_action
+import time
 
 
 def sample_best_minmax_action(game_state):
@@ -100,24 +101,30 @@ class StateNode:
 
 
 def monte_carlo_tree_search(game_state):
+    timeout_start = time.time()
+    timeout = 0.4
     # iterative deepening
     root_state = StateNode(game_state)
     root_state.generate_actions()
     max_depth = 6 - len(game_state['snake_heads'])
+    depth = 1
+    iteration_counter = 0
+    iterations_per_depth = 3
     # discounting factor
     alpha = 0.6
-    each_depth_iteration = 1
-    for i in range(each_depth_iteration):
-        for depth in range(1, max_depth + 1):
-            leaf_state, accumulated_reward = traverse(root_state, max_depth)
-            simulation_result = get_simulation_result(
-                leaf_state) + accumulated_reward
-            backpropagate(leaf_state, simulation_result, alpha)
-            #for action in root_state.actions:
-            #    print(action.action, action.get_min_state().value,
-            #          action.get_min_state().reward)
-    # print action values
-
+    while time.time() < timeout_start + timeout:
+        leaf_state, accumulated_reward = traverse(root_state, depth)
+        simulation_result = get_simulation_result(
+            leaf_state) + accumulated_reward
+        backpropagate(leaf_state, simulation_result, alpha)
+        iteration_counter += 1
+        if iteration_counter > iterations_per_depth:
+            iteration_counter = 0
+            depth += 1
+        #for action in root_state.actions:
+        #    print(action.action, action.get_min_state().value,
+        #          action.get_min_state().reward)
+    #print(depth)
     return root_state.get_max_action().action
 
 
@@ -149,7 +156,7 @@ def backpropagate(state, result, alpha):
     backpropagate(state.parent_action.parent_state, result, alpha)
 
 
-#state = {'height': 11, 'width': 11, 'food': {(1, 6), (10, 8)}, 'hazards': {(4, 0), (5, 4), (5, 1), (5, 7), (9, 5), (5, 10), (10, 0), (10, 6), (0, 5), (1, 0), (10, 9), (6, 5), (4, 5), (5, 0), (5, 6), (5, 3), (5, 9), (9, 10), (0, 1), (10, 5), (0, 4), (0, 10), (1, 5), (6, 10), (3, 5), (4, 10), (9, 0), (5, 5), (0, 0), (10, 4), (10, 1), (0, 9), (0, 6), (10, 10), (1, 10), (6, 0), (7, 5)}, 'snake_heads': [(2, 9), (9, 7)], 'snake_bodies': [[(3, 9), (4, 9), (4, 8), (4, 7), (4, 6), (3, 6), (3, 7), (2, 7)], [(9, 8), (8, 8), (8, 7), (8, 6), (8, 5), (8, 4), (8, 3), (8, 2)]], 'snake_lengths': [9, 9], 'snake_healths': [95, 85]}
+#state = {'height': 11, 'width': 11, 'food': {(6, 7), (9, 8), (6, 6), (1, 6), (1, 9)}, 'hazards': {(4, 0), (5, 4), (5, 1), (5, 7), (9, 5), (5, 10), (10, 0), (10, 6), (0, 5), (1, 0), (10, 9), (6, 5), (4, 5), (5, 0), (5, 6), (5, 3), (5, 9), (9, 10), (0, 1), (10, 5), (0, 4), (0, 10), (1, 5), (6, 10), (3, 5), (4, 10), (9, 0), (5, 5), (0, 0), (10, 4), (10, 1), (0, 9), (0, 6), (10, 10), (1, 10), (6, 0), (7, 5)}, 'snake_heads': [(2, 9), (2, 4)], 'snake_bodies': [[(2, 8), (3, 8), (4, 8), (5, 8), (6, 8), (6, 9), (7, 9), (7, 10)], [(3, 4), (4, 4), (4, 3), (4, 2), (5, 2), (6, 2), (6, 1), (7, 1), (7, 2), (7, 3)]], 'snake_lengths': [9, 11], 'snake_healths': [95, 90]}
 #state['snake_heads'][0], state['snake_heads'][1] = state['snake_heads'][1], state['snake_heads'][0]
 #state['snake_bodies'][0], state['snake_bodies'][1] = state['snake_bodies'][1], state['snake_bodies'][0]
 #state['snake_lengths'][0], state['snake_lengths'][1] = state['snake_lengths'][1], state['snake_lengths'][0]
