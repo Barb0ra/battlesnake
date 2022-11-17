530b25443dbdd11b22b80372efb4ef26a6bdd06d
Barbora Barancikova <2439071b@student.gla.ac.uk>
2022-11-17 17:45:12 +0000

    Add small fixes after the rebase

--- heuristic_baseline_functions/one_step_lookahead.py
+++ heuristic_baseline_functions/one_step_lookahead.py
@@ -1,7 +1,7 @@
 import itertools
 import copy
-from state_reward_heuristic import state_reward
-from state_value import state_value_deterministic
+from heuristic_baseline_functions.state_reward_heuristic import state_reward
+from heuristic_baseline_functions.state_value import state_value_deterministic
 from state_generator import next_state_for_action
 
 
