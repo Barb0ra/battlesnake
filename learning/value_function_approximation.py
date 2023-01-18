a848084bfea7b622deb12e371e89705787dc1be2
Barb0ra <bbarancikova@gmail.com>
2023-01-18 22:28:59 +0100

    Add alpha noise term to GP value function approximation

--- learning/value_function_approximation.py
+++ learning/value_function_approximation.py
@@ -7,6 +7,7 @@ from learning.run_games import run_games
 
 GO_FILE_PATH = '../battlesnake_rules/battlesnake'
 GAME_LOGS_FILE_PATH = 'learning/state_values.txt'
+VALUE_FUNCTION_FILE_PATH = 'learning/value_functions/'
 
 
 def load_game_state_data():
@@ -21,8 +22,8 @@ def load_game_state_data():
         # convert to float
         split_line = [float(x) for x in split_line]
         state_vectors.append(split_line[:6])
-        values.append(split_line[7])
-        rewards.append(split_line[8])
+        values.append(split_line[6])
+        rewards.append(split_line[7])
 
     # each game is separated by a terminal state: win '-111, -111, -111, -111, 1.0, 1.0\n' or lose '-111, -111, -111, -111, -1.0, -1.0\n'
     game_end_indices = []
@@ -64,7 +65,7 @@ def get_td_targets_per_game(state_vectors, values, rewards):
     return td_lambda_targets
 
 
-def train_gp(state_vectors, td_lambda_targets):
+def train_gp(state_vectors, td_lambda_targets, alpha):
     # train a Gaussian Process to predict the td_lambda_targets
     gp_value_fucntion = GaussianProcessRegressor()
     gp_value_fucntion.fit(state_vectors, td_lambda_targets)
@@ -101,7 +102,7 @@ def get_td_targets(state_vectors, values, rewards, game_end_indices):
     return joined_state_vectors, td_lambda_targets
 
 
-def learn_first_batch(model_name, games_per_batch, sampling_rate):
+def learn_first_batch(model_name, games_per_batch, sampling_rate, starting_alpha):
     # there are no pre-generated data and we need to start from a basic value function
     # play a game
     snakes = [{'name': 'Bayesian', 'type': 'tree_search', 'port': 8000, 'params': [2.0, 8.0, 6.0, 0.9,
@@ -114,21 +115,24 @@ def learn_first_batch(model_name, games_per_batch, sampling_rate):
         state_vectors, values, rewards, game_end_indices)
     state_vectors, td_lambda_targets = sample_from_training_data(
         sampling_rate, state_vectors, td_lambda_targets)
+    alphas = np.full(len(state_vectors), starting_alpha)
     # train a Gaussian Process to predict the td_lambda_targets
-    gp_value_fucntion = train_gp(state_vectors, td_lambda_targets)
+    gp_value_fucntion = train_gp(state_vectors, td_lambda_targets, alphas)
     # save the data
-    np.save('learning/value_functions/state_vectors.npy', state_vectors)
-    np.save('learning/value_functions/td_lambda_targets.npy', td_lambda_targets)
+    np.save(VALUE_FUNCTION_FILE_PATH + 'state_vectors_' + model_name + '.npy', state_vectors)
+    np.save(VALUE_FUNCTION_FILE_PATH + 'td_lambda_targets_' + model_name + '.npy', td_lambda_targets)
+    np.save(VALUE_FUNCTION_FILE_PATH + 'alphas_' + model_name + '.npy', alphas)
     # save the Gaussian Process
     joblib.dump(gp_value_fucntion, model_name)
     return gp_value_fucntion
 
 
-def learn_batch(model_name, games_per_batch, sampling_rate):
+def learn_batch(model_name, games_per_batch, sampling_rate, alpha):
     # load the old data
-    state_vectors = np.load('learning/value_functions/state_vectors.npy')
+    state_vectors = np.load(VALUE_FUNCTION_FILE_PATH + 'state_vectors_' + model_name + '.npy')
     td_lambda_targets = np.load(
-        'learning/value_functions/td_lambda_targets.npy')
+        VALUE_FUNCTION_FILE_PATH + 'td_lambda_targets_' + model_name + '.npy')
+    alphas = np.load(VALUE_FUNCTION_FILE_PATH + 'alphas_' + model_name + '.npy')
     # play a game
     snakes = [{'name': 'Bayesian', 'type': 'tree_search', 'port': 8000, 'params': [2.0, 8.0, 6.0, 0.9,
                                                                                    'lcb', True, 'basic']}, {'name': 'Baseline', 'type': 'one_step_lookahead', 'port': 8080, 'params': []}]
@@ -144,11 +148,13 @@ def learn_batch(model_name, games_per_batch, sampling_rate):
     state_vectors = np.concatenate((state_vectors, new_state_vectors))
     td_lambda_targets = np.concatenate(
         (td_lambda_targets, new_td_lambda_targets))
+    alphas = np.concatenate((alphas, np.full((len(new_state_vectors)), alpha)))
     # train a Gaussian Process to predict the td_lambda_targets
-    gp_value_fucntion = train_gp(state_vectors, td_lambda_targets)
+    gp_value_fucntion = train_gp(state_vectors, td_lambda_targets, alphas)
     # save the data
-    np.save('learning/value_functions/state_vectors.npy', state_vectors)
-    np.save('learning/value_functions/td_lambda_targets.npy', td_lambda_targets)
+    np.save(VALUE_FUNCTION_FILE_PATH + 'state_vectors.npy', state_vectors)
+    np.save(VALUE_FUNCTION_FILE_PATH + 'td_lambda_targets.npy', td_lambda_targets)
+    np.save(VALUE_FUNCTION_FILE_PATH +'alphas.npy', alphas)
     # save the Gaussian Process
     joblib.dump(gp_value_fucntion, model_name)
     return gp_value_fucntion
@@ -165,15 +171,16 @@ def sample_from_training_data(sampling_rate, state_vectors, td_lambda_targets):
     return np.array(sampled_state_vectors), np.array(sampled_td_lambda_targets)
 
 
-def learn_value_function(model_name, num_batches, games_per_batch, sampling_rate):
+def learn_value_function(model_name, num_batches, games_per_batch, sampling_rate, starting_alpha, alpha_step_size):
     # learn the value function
     delete_old_game_state_data()
     gp_value_fucntion = learn_first_batch(
-        model_name, games_per_batch, sampling_rate)
+        model_name, games_per_batch, sampling_rate, starting_alpha)
     for i in range(num_batches-1):
+        alpha = starting_alpha - alpha_step_size * (i+1)
         delete_old_game_state_data()
         gp_value_fucntion = learn_batch(
-            model_name, games_per_batch, sampling_rate)
+            model_name, games_per_batch, sampling_rate, alpha)
     return gp_value_fucntion
 
 
